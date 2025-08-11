a8145502ee0aeef54775391239a5b35b0cf2255f
Frida Baharizki <fridabaharizki@gmail.com>
2025-08-11 17:27:27 +0700

    Add TypeScript as a dev dependency and add sample tests

---
+++ test/sample.test.ts
@@ -0,0 +1,10 @@
+import { describe, expect, it } from "vitest";
+
+describe('Sample Test', () => {
+    it('should pass', () => {
+        expect(1).toBe(1);
+    });
+    it('should pass 2', () => {
+        expect(2).toBe(2);
+    });
+});
